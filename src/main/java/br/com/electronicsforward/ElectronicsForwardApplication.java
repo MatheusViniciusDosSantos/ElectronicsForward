b18adf4c6415764d73986a3f1a3782acb8fc7f9c
MatheusViniciusDosSantos <matheus.vinicius77@hotmail.com>
2022-05-30 10:06:39 -0300

    Iniciando projeto Spring, Electronics Forward

---
+++ src/main/java/br/com/electronicsforward/ElectronicsForwardApplication.java
@@ -0,0 +1,13 @@
+package br.com.electronicsforward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsForwardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicsForwardApplication.class, args);
+    }
+
+}
